3235accd91937de4afe1885dd5b76156093d9e18
고현영 <hyunyoungko@gohyeon-yeong-ui-MacBookAir.local>
2022-06-10 23:51:32 +0900

    ADD: category and product api testing

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,53 @@
-from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from rest_framework.test import APITestCase
+
+from products.models import Category, Product, Feeling, SkinType, Ingredient
+
+
+class CategoryTestCase(APITestCase):
+    def setUp(self):
+        self.category = Category.objects.create(
+                                    category_name    = 'test category',
+                                    main_description = 'test main description',
+                                    sub_description  = 'test sub description'
+                                )
+
+    def test_category_list_get(self):
+        response = self.client.get(reverse('category-list'))
+        self.assertEqual(response.status_code, 200)
+    
+    def test_category_detail_get(self):
+        response = self.client.get(reverse('category-detail', args=(self.category.id, )))
+        self.assertEqual(response.status_code, 200)
+
+
+class ProductTestCase(APITestCase):
+    def setUp(self):
+        self.category = Category.objects.create(
+                                    category_name    = 'test category',
+                                    main_description = 'test main description',
+                                    sub_description  = 'test sub description'
+                                )
+
+        self.feeling    = Feeling.objects.create(name='test feeling')
+        self.skin_type  = SkinType.objects.create(name='test skin type')
+        self.ingredient = Ingredient.objects.create(name='test ingredient')
+
+        self.product = Product.objects.create(
+            name        = 'test name',
+            price       = 1000.00,
+            size        = 'test size',
+            description = 'test description',
+            category    = self.category,
+            howtouse    = {'test':'test'},
+            badge       = 'test badge'
+        )
+
+    def test_product_list_get(self):
+        response = self.client.get(reverse('product-list'))
+        self.assertEqual(response.status_code, 200)
+    
+    def test_product_detail_get(self):
+        response = self.client.get(reverse('product-detail', args=(self.product.id, )))
+        self.assertEqual(response.status_code, 200)
--- products/urls.py
+++ products/urls.py
@@ -4,19 +4,11 @@ from products.views import (CategoryListGV, CategoryDetailGV,ProductDetailGV, Pr
                             ReviewListGV, ReviewCreateGV, ReviewDetailGV)
 
 urlpatterns = [
-    # path('/categories', CategoryListView.as_view()),
-    # path('/categories/<int:category_id>', CategoryDetailView.as_view()),
-    # path('/recommend/<int:product_id>' , RecommendedView.as_view()),
-
-    path('/categories/<int:pk>', CategoryDetailGV.as_view()),
-    path('/categories', CategoryListGV.as_view()),
-    path('', ProductListGV.as_view()),
+    path('/categories/<int:pk>', CategoryDetailGV.as_view(), name='category-detail'),
+    path('/categories', CategoryListGV.as_view(), name='category-list'),
+    path('', ProductListGV.as_view(), name='product-list'),
     path('/<int:pk>' , ProductDetailGV.as_view(), name='product-detail'),
-
-    path('/<int:pk>/reviews' , ReviewListGV.as_view()),
-    path('/review/<int:pk>' , ReviewDetailGV.as_view()),
-    path('/<int:pk>/review-create' , ReviewCreateGV.as_view()),
-
-    # path('/<int:product_id>/review' , ProductReviewView.as_view()),
-    # path('/review/<int:review_id>' , ProductReviewView.as_view())
+    path('/<int:pk>/reviews' , ReviewListGV.as_view(), name='review-list'),
+    path('/review/<int:pk>' , ReviewDetailGV.as_view(), name='review-detail'),
+    path('/<int:pk>/review-create' , ReviewCreateGV.as_view(), name='review-create'),
 ]
